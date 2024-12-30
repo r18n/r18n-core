6fb6f09a9c55b0be6d0ea3ba3bbfaf526d0c1601
Alexander Popov <alex.wayfer@gmail.com>
2024-12-30 22:11:14 +0300

    Add specs for Swedish locales titles

--- spec/locales/sv_spec.rb
+++ spec/locales/sv_spec.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 describe R18n::Locales::Sv do
+  it 'has correct title' do
+    sv = R18n.locale('sv')
+    expect(sv.title).to eq('Svenska')
+  end
+
   it 'formats Swedish date' do
     sv = R18n::I18n.new('sv')
     expect(sv.l(Date.parse('2009-05-01'), :full)).to eq('1 maj 2009')
