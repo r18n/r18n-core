4e7177c270c1185c243b8d90cce0b627dbf67b93
Alexander Popov <alex.wayfer@gmail.com>
2024-12-30 22:07:53 +0300

    Add Swedish locale specs

---
+++ spec/locales/sv_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+describe R18n::Locales::Sv do
+  it 'formats Swedish date' do
+    en = R18n::I18n.new('sv')
+    expect(en.l(Date.parse('2009-05-01'), :full)).to eq('1 maj 2009')
+    expect(en.l(Date.parse('2009-05-01'), :full, year: false)).to eq('1 maj')
+  end
+end
