b4243b9a2a801d19d49618d686b238ad44c2fac5
Alexander Popov <alex.wayfer@gmail.com>
2024-12-30 22:06:13 +0300

    Remove English from Swedish sublocales

--- lib/r18n-core/locales/sv.rb
+++ lib/r18n-core/locales/sv.rb
@@ -6,7 +6,7 @@ module R18n
     class Sv < Locale
       set(
         title: 'Svenska',
-        sublocales: %w[sv en],
+        sublocales: %w[sv],
 
         wday_names: %w[söndag måndag tisdag onsdag torsdag fredag lördag],
         wday_abbrs: %w[sön mån tis ons tor fre lör],
