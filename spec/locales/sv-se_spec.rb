eca2d4645acd33d6790f9d0b285745fa0a7c8871
Alexander Popov <alex.wayfer@gmail.com>
2024-12-30 22:09:57 +0300

    Fix variables names for Swedish locale specs

--- spec/locales/sv-se_spec.rb
+++ spec/locales/sv-se_spec.rb
@@ -2,8 +2,8 @@
 
 describe R18n::Locales::SvSE do
   it 'formats Swedish (Sweden) date' do
-    en = R18n::I18n.new('sv-se')
-    expect(en.l(Date.parse('2009-05-01'), :full)).to eq('1 maj 2009')
-    expect(en.l(Date.parse('2009-05-01'), :full, year: false)).to eq('1 maj')
+    sv_se = R18n::I18n.new('sv-se')
+    expect(sv_se.l(Date.parse('2009-05-01'), :full)).to eq('1 maj 2009')
+    expect(sv_se.l(Date.parse('2009-05-01'), :full, year: false)).to eq('1 maj')
   end
 end
--- spec/locales/sv_spec.rb
+++ spec/locales/sv_spec.rb
@@ -2,8 +2,8 @@
 
 describe R18n::Locales::Sv do
   it 'formats Swedish date' do
-    en = R18n::I18n.new('sv')
-    expect(en.l(Date.parse('2009-05-01'), :full)).to eq('1 maj 2009')
-    expect(en.l(Date.parse('2009-05-01'), :full, year: false)).to eq('1 maj')
+    sv = R18n::I18n.new('sv')
+    expect(sv.l(Date.parse('2009-05-01'), :full)).to eq('1 maj 2009')
+    expect(sv.l(Date.parse('2009-05-01'), :full, year: false)).to eq('1 maj')
   end
 end
